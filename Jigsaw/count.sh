0230610faa4e4e8f13feb49bd54a07031c313a3c
JoyeeCheung <joyeec9h3@gmail.com>
2014-08-03 01:09:23 +0800

    fix some bugs in [] in count.sh

--- Jigsaw/count.sh
+++ Jigsaw/count.sh
@@ -5,7 +5,7 @@ echo "$1 $2 $3" >> result.txt
 for i in {1..20}; do
     ant run
     result=`grep "No solution" ASearchDialog.txt`
-    if [ ! -z $result ]; then
+    if [ ! -z "$result" ]; then
         echo "-1" >> result.txt
     fi
     result=`grep -o "Total number of searched nodes:[0-9]\+" ASearchDialog.txt | grep -o '[0-9]\+$'`
