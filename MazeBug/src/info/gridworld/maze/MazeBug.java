513d591a5dc0bfcabc72eebc47efd78a65168d64
JoyeeCheung <joyeec9h3@gmail.com>
2014-08-02 19:58:32 +0800

    pass all maps

--- MazeBug/src/info/gridworld/maze/MazeBug.java
+++ MazeBug/src/info/gridworld/maze/MazeBug.java
@@ -3,13 +3,13 @@ package info.gridworld.maze;
 import info.gridworld.actor.Actor;
 import info.gridworld.actor.Bug;
 import info.gridworld.actor.Flower;
-import info.gridworld.grid.*;
+import info.gridworld.actor.Rock;
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
 
 import java.awt.Color;
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.Stack;
-import java.util.HashMap;
 
 import javax.swing.JOptionPane;
 
@@ -17,126 +17,165 @@ import javax.swing.JOptionPane;
  * A <code>MazeBug</code> can find its way in a maze. <br />
  * The implementation of this class is testable on the AP CS A and AB exams.
  */
-public class MazeBug extends Bug {
-	public Location next;
-	public Location last;
-	public boolean isEnd = false;
+public class MazeBug extends Bug
+{
+    public Location next;
+    public Location last;
+    public boolean isEnd = false;
 
     // each ArrayList<Location> contains <src, visiterA, visitedB...(4 dirs)>
     // hence the name cross(+) location.
-	public Stack<ArrayList<Location>> crossLocation = new Stack<ArrayList<Location>>();
-	public Integer stepCount = 0;
-	boolean hasShown = false;//final message has been shown
+    public Stack<ArrayList<Location>> crossLocation = new Stack<ArrayList<Location>>();
+    public Integer stepCount = 0;
+    boolean hasShown = false;//final message has been shown
 
     // number of times the direction is chosen
     // 0 - NORTH, 1 - EAST, 2 - SOUTH, 3 - WEST
     int[] dirCount = { 0, 0, 0, 0 };
-    int[] validDirections = { Location.NORTH, Location.EAST, Location.SOUTH, Location.WEST }
+    int[] validDirections = { Location.NORTH, Location.EAST, Location.SOUTH,
+            Location.WEST };
 
-	/**
-	 * Constructs a box bug that traces a square of a given side length
-	 * 
-	 * @param length
-	 *            the side length
-	 */
-	public MazeBug() {
-		setColor(Color.GREEN);
-		last = new Location(0, 0);
+    /**
+     * Constructs a box bug that traces a square of a given side length
+     * 
+     * @param length
+     *            the side length
+     */
+    public MazeBug()
+    {
+        setColor(Color.GREEN);
 
-        // initialize the count for each valid direction
-        dirCount = new HashMap<int, int>();
-        for (int d : validDirections)
+    }
+
+    /**
+     * Moves to the next location of the square.
+     */
+    @Override
+    public void act()
+    {
+        if (stepCount == 0)
         {
-            dirCount.put(d, 0);
+            last = getLocation();
+            // TODO push <(0,0)> into the stack
+            ArrayList<Location> firstCross = new ArrayList<Location>();
+            firstCross.add(last);
+            crossLocation.push(firstCross);
+            System.out.println("Start at " + last);
         }
-
-        // TODO push <(0,0)> into the stack
-        ArrayList<Location> firstCross = new ArrayList<Location>();
-        firstCross.add(last);
-        crossLocation.push(firstCross);
-	}
-
-	/**
-	 * Moves to the next location of the square.
-	 */
-	public void act() {
-		boolean willMove = canMove();
-		if (isEnd == true) {
-            //to show step count when reach the goal		
-            if (hasShown == false) {
+        
+        boolean willMove = canMove();
+        System.out.println("Step Count: " + stepCount);
+        System.out.println("willMove: " + willMove);
+        
+        if (isEnd == true)
+        {
+            System.out.println("End at " + next);
+            //to show step count when reach the goal
+            if (hasShown == false)
+            {
                 String msg = stepCount.toString() + " steps";
                 JOptionPane.showMessageDialog(null, msg);
                 hasShown = true;
             }
-        } else if (willMove) {
+        }
+        else if (willMove)
+        {
             move();
-            //increase step count when move 
-            stepCount++;
-        } else {  // not the end but can't move
-            back(); 
+            System.out.println("Move to " + next);
+            //increase step count when move
             stepCount++;
         }
-	}
+        else
+        {  // not the end but can't move
+            back();
+            System.out.println("Back to " + next);
+            System.out.println(crossLocation.size());
+            stepCount++;
+        }
 
-	/**
-	 * Find all positions that can be move to.
+    }
+
+    /**
+     * Find all positions that can be move to.
      * Visted locations are excluded.
-	 * 
-	 * @param loc
-	 *            the location to detect.
-	 * @return List of positions.
-	 */
-	public ArrayList<Location> getValid(Location loc) {
-		Grid<Actor> gr = getGrid();
-		if (gr == null)
-			return null;
-		ArrayList<Location> valid = new ArrayList<Location>();
-		
+     * 
+     * @param loc
+     *            the location to detect.
+     * @return List of positions.
+     */
+    public ArrayList<Location> getValid(Location loc)
+    {
+        Grid<Actor> gr = getGrid();
+        if (gr == null)
+        {
+            return null;
+        }
+        ArrayList<Location> valid = new ArrayList<Location>();
+
         // return an empty list if the stack is empty
         if (crossLocation.size() <= 0)
+        {
             return valid;
+        }
 
         // TODO check each of the 4 directions
         for (int d : validDirections)
         {
             Location check = loc.getAdjacentLocation(d);
+//            System.out.println("Check " + check);
 
             // if it is out of the grid, skip
             if (!gr.isValid(check))
+            {
+                //System.out.println(check + " Out of grid");
                 continue;
+            }
 
-            Actor neighbor = (Actor) gr.get(check);
+            Actor neighbor = gr.get(check);
             // TODO if it is null, add it
             if (neighbor == null)
             {
+                //System.out.println(check + " is null");
                 valid.add(check);
             }
             // TODO if it is a red rock, add it
-            else if (neighbor instanceof Rock && neighbor.getColor().equals(Color.RED))
+            else if (neighbor instanceof Rock
+                    && neighbor.getColor().equals(Color.RED))
             {
-                valid.add(check); 
+                //System.out.println(check + " is a red rock");
+                valid.add(check);
             }
+            else
+            {
+                //System.out.println(check + " is " + neighbor);
+            }
+            
+
         }
 
-		return valid;
-	}
+        return valid;
+    }
 
-	/**
-	 * Tests whether this bug can move forward into a location that is empty or
-	 * contains a flower.
-	 * 
-	 * @return true if this bug can move.
-	 */
-	public boolean canMove() {
+    /**
+     * Tests whether this bug can move forward into a location that is empty or
+     * contains a flower.
+     * 
+     * @return true if this bug can move.
+     */
+    @Override
+    public boolean canMove()
+    {
         // TODO get valid adjacent locations
-		Location loc = getLocation();
+        Location loc = getLocation();
         ArrayList<Location> valid = getValid(loc);
 
         // if there is no valid location around
         if (valid.size() <= 0)
         {
             return false;
-        } else {
+        }
+        else
+        {
             return true;
         }
     }
@@ -144,62 +183,69 @@ public class MazeBug extends Bug {
     private void pickNext()
     {
         // TODO get valid adjacent locations
-		Location loc = getLocation();
+        Grid<Actor> gr = getGrid();
+        Location loc = getLocation();
         ArrayList<Location> valid = getValid(loc);
 
         int maxCount = 0;
         for (Location check : valid)
         {
-            Actor neighbor = (Actor) gr.get(check);
+            Actor neighbor = gr.get(check);
             int dirIndex = loc.getDirectionToward(check) / 90;
             // TODO check if the end is in them, move to the end if it is
-            if (neighbor instanceof Rock && neighbor.getColor().equals(Color.RED))
+            if (neighbor instanceof Rock
+                    && neighbor.getColor().equals(Color.RED))
             {
                 last = next;
                 next = check;
                 break;
             }
             // TODO choose the one with max probability
-            if (dirCount[dirIndex]) >= maxCount)  
+            if (dirCount[dirIndex] >= maxCount)
             {
                 maxCount = dirCount[dirIndex];
                 last = next;
                 next = check;
             }
         }
-   
+
     }
 
-	/**
-	 * Moves the bug forward, putting a flower into the location it previously
-	 * occupied.
-	 */
-	public void move() {
-		Grid<Actor> gr = getGrid();
-		if (gr == null)
-			return;
+    /**
+     * Moves the bug forward, putting a flower into the location it previously
+     * occupied.
+     */
+    @Override
+    public void move()
+    {
+        Grid<Actor> gr = getGrid();
+        if (gr == null)
+        {
+            return;
+        }
 
         // pick next location (via field next and last)
         // assert: canMove -- getValid.size != 0 -- next != here
         pickNext();
 
-		if (!gr.isValid(next)) {
-			removeSelfFromGrid();
+        if (!gr.isValid(next))
+        {
+            removeSelfFromGrid();
             return;
         }
 
-        // move to the next location
-        Location loc = getLocation();
-        setDirection(loc.getDirectionToward(next));
-        moveTo(next);
-
         // if the end is reached, mark it
-        Actor nextActor = (Actor) gr.get(next);
+        Actor nextActor = gr.get(next);
         if (nextActor instanceof Rock)
         {
             isEnd = true;
         }
-
+        
+        // move to the next location
+        Location loc = getLocation();
+        setDirection(loc.getDirectionToward(next));
+        moveTo(next);
+        
         // TODO increase the probability for this direction
         int dirIndex = loc.getDirectionToward(next) / 90;
         dirCount[dirIndex]++;
@@ -218,51 +264,56 @@ public class MazeBug extends Bug {
         ArrayList<Location> newTop = new ArrayList<Location>();
         newTop.add(next);
         crossLocation.push(newTop);
-	}
+    }
 
     // TODO drop flower at loc
     private void dropFlower(Location loc)
     {
-		Grid<Actor> gr = getGrid();
-		if (gr == null)
-			return;
-		Flower flower = new Flower(getColor());
-		flower.putSelfInGrid(gr, loc);
+        Grid<Actor> gr = getGrid();
+        if (gr == null)
+        {
+            return;
+        }
+        Flower flower = new Flower(getColor());
+        flower.putSelfInGrid(gr, loc);
     }
 
     // TODO move back to src
     private void back()
     {
-		Grid<Actor> gr = getGrid();
-		if (gr == null)
-			return;
-
-        // the stack is empty, there is no way to go back
-        if (crossLocation.size() <= 0)
-            return;
-
-        Location loc = getLocation();
-
-        // pop out the top(contains current location)   
-        crossLocation.pop();
-
-        // if there is no source, remove self from grid and return
-        if (crossLocation.size() <= 0)
+        Grid<Actor> gr = getGrid();
+        if (gr == null)
         {
-            removeSelfFromGrid();
             return;
         }
 
+        // the stack is empty, there is no way to go back
+        if (crossLocation.size() <= 0)
+        {
+            return;
+        }
+
+        Location loc = getLocation();
+        
+        // pop out the top(contains current location)
+        crossLocation.pop();
+        
+        // the stack is empty, there is no way to go back
+        if (crossLocation.size() <= 0)
+        {
+            return;
+        }
+        
         // get the src of top. which is [0] at
         // the linked list one way below the top
         next = crossLocation.peek().get(0);
         last = loc;
-
-        // drop flower
-        dropFlower(loc);
-
+        
         // move back to src
         setDirection(loc.getDirectionToward(next));
         moveTo(next);
+
+        // drop flower
+        dropFlower(loc);
     }
 }
