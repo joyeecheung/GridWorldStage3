8b387d5213ea6fcf198cb4cacf936ca6426164d2
JoyeeCheung <joyeec9h3@gmail.com>
2014-08-12 17:05:21 +0800

    add sawp for mazebug

---
+++ MazeBug/swap.sh
@@ -0,0 +1,3 @@
+mv src/info/gridworld/maze/MazeBug.java src/info/gridworld/maze/tmp
+mv src/info/gridworld/maze/MazeBug.java2 src/info/gridworld/maze/MazeBug.java
+mv src/info/gridworld/maze/tmp src/info/gridworld/maze/MazeBug.java2
